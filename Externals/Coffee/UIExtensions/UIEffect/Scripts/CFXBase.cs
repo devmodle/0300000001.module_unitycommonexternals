3a12fed3b0878360c78d8d285a25dafe4029c1b6
sd.lee <sd.lee@ninetap.com>
2020-11-13 19:28:45 +0900

    [이상동] UIEffect 부모 클래스 변경

---
+++ Externals/Coffee/UIExtensions/UIEffect/Scripts/CFXBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Coffee.UIExtensions {
+	//! 효과
+	public abstract class CFXBase : UIEffectBase {
+		#region 프로퍼티
+		public abstract float effectFactor { get; set; }
+		#endregion			// 프로퍼티
+	}
+}
